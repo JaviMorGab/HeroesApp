2ad51b3336158e172173f990042d65a8a9ac9d71
Francisco Javier Moreno Gabarri <fjmoreno@isotrol.net>
2021-12-13 10:26:15 +0100

    fifth commit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+
 
 
 
@@ -16,7 +18,8 @@ import { RegisterComponent } from './pages/register/register.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+
+  constructor(private authService: AuthService,
+              private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+      return this.authService.verigicaAutentificacion()
+        .pipe(
+          tap(estaAutenticado => {
+            if(!estaAutenticado) {
+              this.router.navigate(['./auth/login']);
+            }
+          })
+        )
+    //   if(this.authService.auth.id) {
+    //     return true;
+    //   }
+    // return false;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | boolean {
+
+      return this.authService.verigicaAutentificacion()
+        .pipe(
+         tap(estaAutenticado => {
+           if(!estaAutenticado) {
+              this.router.navigate(['./auth/login']);
+           }
+          })
+        )
+    //   if(this.authService.auth.id) {
+    //     return true;
+    //   }
+    // return false;
+  }
+}
